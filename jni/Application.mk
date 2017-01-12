e0364622e453b9619f6667e7af3cba879e59ab56
EXL <exlmotodev@gmail.com>
2017-01-12 10:52:37 +0000

    Enable on emulator

--- jni/Application.mk
+++ jni/Application.mk
@@ -3,7 +3,7 @@
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 # APP_STL := stlport_static
 
-APP_ABI := armeabi-v7a
+APP_ABI := armeabi
 #APP_ABI := armeabi armeabi-v7a x86
 
 # Min SDK level
