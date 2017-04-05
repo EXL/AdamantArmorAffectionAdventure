965777de5eeefacf554e336aa9dead523cf0cfd5
EXL <exlmotodev@gmail.com>
2017-04-05 10:30:05 +0700

    Add x86 and arm archs

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,11 +1,8 @@
-
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 # APP_STL := stlport_static
 
-APP_ABI := armeabi-v7a
-#APP_ABI := armeabi armeabi-v7a x86
+APP_ABI := armeabi armeabi-v7a x86
 
 # Min SDK level
 APP_PLATFORM=android-10
-
